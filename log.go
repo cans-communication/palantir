45c615e8657f333db87472af344bf4e1caf018d3
tarcanscloud <tar@canscloud.cc>
2025-01-08 06:28:08 +0000

    refactor: singleton

--- log.go
+++ log.go
@@ -3,98 +3,119 @@ package palantir
 import (
 	"context"
 	"os"
-	"unicode/utf8"
 
 	"golang.org/x/exp/slog"
 )
 
+type ValueType int
+
+const (
+	ValueTypeString ValueType = iota
+	ValueTypeInt
+	ValueTypeFloat64
+	ValueTypeBool
+)
+
 // Attr defines a key-value pair for log attributes
 type Attr struct {
-	key string
-	val interface{}
+	valueType ValueType
+	key       string
+	val       interface{}
 }
 
 // String creates a string attribute
 func String(k, v string) Attr {
-	return Attr{k, v}
+	return Attr{ValueTypeString, k, v}
 }
 
 // Int creates an integer attribute
 func Int(k string, v int) Attr {
-	return Attr{k, v}
+	return Attr{ValueTypeInt, k, v}
 }
 
 // Float64 creates a float64 attribute
 func Float64(k string, v float64) Attr {
-	return Attr{k, v}
+	return Attr{ValueTypeFloat64, k, v}
 }
 
 // Bool creates a boolean attribute
 func Bool(k string, v bool) Attr {
-	return Attr{k, v}
+	return Attr{ValueTypeBool, k, v}
 }
 
-// ILogger defines the logging interface
-type ILogger interface {
-	Info(ctx context.Context, message string, attrs ...Attr)
-	Debug(ctx context.Context, message string, attrs ...Attr)
-	Warn(ctx context.Context, message string, attrs ...Attr)
-	Error(ctx context.Context, err error, attrs ...Attr)
-}
+var slogger *slog.Logger
 
-// Logger wraps slog.Logger for custom logging
-type Logger struct {
-	slogger *slog.Logger
-}
+func init() {
 
-// NewLogger initializes a new Logger
-func NewLogger() *Logger {
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
-	return &Logger{slogger: slog.New(handler)}
+	level := slog.LevelInfo
+
+	switch os.Getenv("LOG_LEVEL") {
+	case "DEBUG":
+		level = slog.LevelDebug
+	case "WARN":
+		level = slog.LevelWarn
+	case "ERROR":
+		level = slog.LevelError
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+
+	slogger = slog.New(handler)
 }
 
 // Info logs an informational message
-func (l *Logger) Info(ctx context.Context, message string, attrs ...Attr) {
-	l.log(ctx, slog.LevelInfo, message, attrs...)
+func Info(ctx context.Context, message string, attrs ...Attr) {
+	log(ctx, slog.LevelInfo, message, attrs...)
 }
 
 // Debug logs a debug message
-func (l *Logger) Debug(ctx context.Context, message string, attrs ...Attr) {
-	l.log(ctx, slog.LevelDebug, message, attrs...)
+func Debug(ctx context.Context, message string, attrs ...Attr) {
+	log(ctx, slog.LevelDebug, message, attrs...)
 }
 
 // Warn logs a warning message
-func (l *Logger) Warn(ctx context.Context, message string, attrs ...Attr) {
-	l.log(ctx, slog.LevelWarn, message, attrs...)
+func Warn(ctx context.Context, message string, attrs ...Attr) {
+	log(ctx, slog.LevelWarn, message, attrs...)
 }
 
 // Error logs an error message
-func (l *Logger) Error(ctx context.Context, err error, attrs ...Attr) {
-	errorAttr := Attr{"error", err.Error()}
-	l.log(ctx, slog.LevelError, "Error: "+err.Error(), append(attrs, errorAttr)...)
+func Error(ctx context.Context, err error, attrs ...Attr) {
+	log(ctx, slog.LevelError, "Error: "+err.Error(), attrs...)
 }
 
 // log is a helper function to log messages at the specified level
-func (l *Logger) log(ctx context.Context, level slog.Level, message string, attrs ...Attr) {
-	attributes := make([]any, 0, len(attrs)*2)
-	textValue := ""
+func log(ctx context.Context, level slog.Level, message string, attrs ...Attr) {
 
-	for _, attr := range attrs {
-		attributes = append(attributes, attr.key, attr.val)
-		if attr.key == "text" {
-			if text, ok := attr.val.(string); ok {
-				textValue = text
-			}
-		}
+	if slogger == nil {
+		return
 	}
 
-	// If text is provided, calculate and append bytes_count and char_count
-	if textValue != "" {
-		attributes = append(attributes,
-			"bytes_count", len(textValue),
-			"char_count", utf8.RuneCountInString(textValue),
+	sattrs := make([]any, 0, len(attrs))
+
+	for _, attr := range attrs {
+		var sval slog.Value
+
+		switch attr.valueType {
+		case ValueTypeString:
+			val, _ := attr.val.(string)
+			sval = slog.StringValue(val)
+		case ValueTypeInt:
+			val, _ := attr.val.(int)
+			sval = slog.IntValue(val)
+		case ValueTypeFloat64:
+			val, _ := attr.val.(float64)
+			sval = slog.Float64Value(val)
+		case ValueTypeBool:
+			val, _ := attr.val.(bool)
+			sval = slog.BoolValue(val)
+		default:
+			sval = slog.StringValue("")
+		}
+
+		sattrs = append(sattrs,
+			slog.Attr{Key: attr.key, Value: sval},
 		)
 	}
 
-	l.slogger.Log(ctx, level, message, attributes...)
+	slogger.Log(ctx, level, message, sattrs...)
 }
--- log_test.go
+++ log_test.go
@@ -1,41 +1,41 @@
-package palantir
+package palantir_test
 
 import (
 	"context"
 	"testing"
+
+	"github.com/cans-communication/palantir"
 )
 
 func TestLogger(t *testing.T) {
-	logger := NewLogger()
-
 	ctx := context.Background()
 
 	// Info log test
-	logger.Info(ctx, "Test log message",
-		String("text", "Test text ทดสอบ"),
-		String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
-		String("ref", "TestRef #1"),
+	palantir.Info(ctx, "Test log message",
+		palantir.String("text", "Test text ทดสอบ"),
+		palantir.String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
+		palantir.String("ref", "TestRef #1"),
 	)
 
 	// Debug log test
-	logger.Debug(ctx, "Test debug message",
-		String("text", "Debug text ทดสอบ"),
-		String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
-		String("ref", "DebugRef #2"),
+	palantir.Debug(ctx, "Test debug message",
+		palantir.String("text", "Debug text ทดสอบ"),
+		palantir.String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
+		palantir.String("ref", "DebugRef #2"),
 	)
 
 	// Warn log test
-	logger.Warn(ctx, "Test warning message",
-		String("text", "Warning text ทดสอบ"),
-		String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
-		String("ref", "WarnRef #3"),
+	palantir.Warn(ctx, "Test warning message",
+		palantir.String("text", "Warning text ทดสอบ"),
+		palantir.String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
+		palantir.String("ref", "WarnRef #3"),
 	)
 
 	// Error log test
-	logger.Error(ctx, context.DeadlineExceeded,
-		String("text", "Error text ทดสอบ"),
-		String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
-		String("ref", "ErrorRef #4"),
+	palantir.Error(ctx, context.DeadlineExceeded,
+		palantir.String("text", "Error text ทดสอบ"),
+		palantir.String("domain_id", "123e4567-e89b-12d3-a456-426614174000"),
+		palantir.String("ref", "ErrorRef #4"),
 	)
 
 	t.Log("Logger tested successfully")
